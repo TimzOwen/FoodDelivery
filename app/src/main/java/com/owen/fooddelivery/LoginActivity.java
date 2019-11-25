5a47f18b92ccb7050a82fe0540d321974434707c
TimzOwen <ktimothy@kabarak.ac.ke>
2019-11-25 14:46:10 +0300

    Updated the Database dependencies and authenticatio

--- app/src/main/java/com/owen/fooddelivery/LoginActivity.java
+++ app/src/main/java/com/owen/fooddelivery/LoginActivity.java
@@ -1,14 +1,61 @@
 package com.owen.fooddelivery;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private Button btnLogin;
+    private EditText etPassword, etPhone;
+    private FirebaseAuth mAuth;
+    private FirebaseDatabase database;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        database = FirebaseDatabase.getInstance();
+        final DatabaseReference table_user = database.getReference("User");
+
+        etPhone = findViewById(R.id.et_input_email_number);
+        etPassword = findViewById(R.id.et_input_password_number);
+        btnLogin = findViewById(R.id.btn_loginpg_join);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                table_user.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot)
+                    {
+
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError)
+                    {
+
+                    }
+                });
+            }
+        });
+
+
+
     }
 }
--- app/src/main/java/com/owen/fooddelivery/MainActivity.java
+++ app/src/main/java/com/owen/fooddelivery/MainActivity.java
@@ -2,18 +2,34 @@ package com.owen.fooddelivery;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button btnLogin;
-    private EditText editText;
-
+    private Button btnLogin, btnSignup;
 
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnLogin = findViewById(R.id.btn_main_login);
+        btnSignup = findViewById(R.id.btn_main_join);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+            }
+        });
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, SignUpActivity.class));
+            }
+        });
     }
 }

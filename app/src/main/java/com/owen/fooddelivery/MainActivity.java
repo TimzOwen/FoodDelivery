e512183d5b3996e8de7a6054c1c330bc38406f78
TimzOwen <ktimothy@kabarak.ac.ke>
2019-11-25 14:00:40 +0300

    updated the UI for Login, Main and sign up activity

--- app/src/main/java/com/owen/fooddelivery/MainActivity.java
+++ app/src/main/java/com/owen/fooddelivery/MainActivity.java
@@ -3,14 +3,16 @@ package com.owen.fooddelivery;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
+    private Button btnLogin;
+
+
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
     }
 }

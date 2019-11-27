ed0b0b76872c4bdd13686951ac6473d1609adf43
Timz Owen <ktimothy@kabarak.ac.ke>
2019-11-27 23:58:38 +0300

    Update SignUpActivity.java

--- app/src/main/java/com/owen/fooddelivery/SignUpActivity.java
+++ app/src/main/java/com/owen/fooddelivery/SignUpActivity.java
@@ -5,10 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class SignUpActivity extends AppCompatActivity {
+    private Button btnCreateAccount;
+    private EditText etName, etPassword , etPhone;
+    private ProgressDialog mDialog;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        //Instanciate Firebase
+        //findIds
+        //CreateAcount
     }
 }
